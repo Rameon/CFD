332aa8e75095eb7a04b1c9c95ca5434b49c0a4a9
Rameon <Yirameon@gmail.com>
2020-09-16 21:29:17 +0900

    Create fpos.m

---
+++ Project_Dream/fpos.m
@@ -0,0 +1,17 @@
+%% CALCULATE POSITIVE FLUX VECTOR %%
+function f = fpos(V,n)
+
+rho = V(1);     % Density
+u   = V(2);     % u velocity
+v   = V(3);     % v velocity
+P   = V(4);     % Pressure
+
+gamma = 1.4;                   
+a     = speedsound(P,rho);
+conV  = u*n(1) + v*n(2);
+M     = conV/a;
+
+f(1)  = rho*a/4*(M+1)^2;
+f(2) = f(1) * (u + n(1)*(-conV+2*a)/gamma);
+f(3) = f(1) * (v + n(2)*(-conV+2*a)/gamma);
+f(4) = f(1) * (h_0(V) - a^2*(M-1)^2/(gamma + 1));
