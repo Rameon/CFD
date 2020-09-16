cd6c577b27c80f7da33b54dd4f997bf6d2c33edd
Rameon <Yirameon@gmail.com>
2020-09-16 21:29:28 +0900

    Create convV_U.m

---
+++ Project_Dream/convV_U.m
@@ -0,0 +1,13 @@
+%% CALCULATE CONSERVATIVE STATE VECTOR %%
+function U = convV_U(V)
+
+rho = V(1);     % Density
+u   = V(2);     % u velocity
+v   = V(3);     % v velocity
+P   = V(4);     % Pressure
+
+% U = [rho rho*u_vel rho*v_vel rho*energy]
+U(1) = rho;
+U(2) = rho*u;
+U(3) = rho*v;
+U(4) = rho*e_0(V);
