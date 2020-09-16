2c31c683c15f0b1f0d3faa42bb8d02396da2c362
Rameon <Yirameon@gmail.com>
2020-09-16 21:29:31 +0900

    Create convU_V.m

---
+++ Project_Dream/convU_V.m
@@ -0,0 +1,13 @@
+%% CALCULATE PRIMITIVE STATE VECTOR %%
+function V = convU_V(U)
+
+rho   = U(1);   % Density
+rho_u = U(2);   % Density * u vel.
+rho_v = U(3);   % Density * v vel.
+rho_e = U(4);   % Density * total energy
+
+% V = [rho u_vel v_vel p]
+V(1) = rho;
+V(2) = rho_u/rho;
+V(3) = rho_v/rho;
+V(4) = (rho_e - rho/2*(V(2)^2+V(3)^2))*(1.4-1);
